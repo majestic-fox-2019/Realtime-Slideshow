13c3030c2e588a3e23343373a5dafe3436795974
Onesinus SPT <onesinus231@gmail.com>
2020-02-21 09:22:13 +0700

    Membuat gambar fullscreen, tinggal letakin title dan content ditengah gambar

--- client/src/components/SlideShow.vue
+++ client/src/components/SlideShow.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <h1>{{total}}</h1>
-    <button @click="add">+</button>
-    <button @click="subtract">-</button>
+    <img :src="'images/'+ total +'.jpg'">
+
+    <div class="content">
+      <button @click="subtract">Prev Slide</button>
+      <button @click="add">Next Slide</button>
+    </div>
   </div>
 </template>
 
@@ -14,7 +17,7 @@ export default {
  name: 'SlideShow',
  data() {
    return {
-     total: 0,
+     total: 1,
    }
  },
  methods: {
@@ -33,11 +36,31 @@ export default {
    });
 
    socket.on('subtract', (newTotal) => {
-     this.total = newTotal;
+     if (newTotal >= 0) {      
+       this.total = newTotal;
+     }
    });
  }
 }
 </script>
+<style>
+  html,body{
+      margin:0;
+      max-height:100%;
+      overflow: hidden;
+      color: white;
+  }
 
-<style scoped>
+  img{
+    display:block;
+    width:100%; 
+    height:100%;
+    object-fit: cover;
+  }
+
+  .content{
+    position: absolute;
+    bottom: 5%;
+    left: 45%;
+  }
 </style>
